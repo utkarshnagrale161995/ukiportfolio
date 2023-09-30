f3a43dee3d4cbd76fca4c28c0b44f46e259a6786
Utkarsh Manikrao Nagrale <utkarsh.nagrale@gmail.com>
2023-09-30 15:02:06 +0530

    #Changes

--- src/component/About.js
+++ src/component/About.js
@@ -1,9 +1,67 @@
 import React from "react";
+import newdeveloper from '../assets/newdeveloper.jpg';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function About(){
+    const image={
+        maxWidth:"50%"
+    }
     return(
         <>
-        <div><h1>You are in About page</h1></div>
+        <div className="container mt-5">
+            <div className="card bg-warning w-75 mx-auto">
+            <div className="card-header "><p className="text-center"><h1>About Me</h1></p></div>
+            <div className="card-body text-center">
+                <div><img src={newdeveloper} alt="developer" style={image}/></div>
+            </div>
+            <div className="card-footer"><b className="text-justify">I am a fullstack developer currently working as a System Engineer at Infosys. I am passionate to 
+                build beautiful and user-friendly website. I have a strong understanding of both
+                front-end and back-end development, and can use my skills in a professional setting. I am 
+                a quick learner and a team player, and i am confident that i can be a valuable
+                asset to any web development team.</b>
+            </div>
+        </div>
+        
+        <br/>
+        <div className="text-center"><h2>My Skills</h2></div>
+        <div className="card mt-5 mx-auto w-75 border border-white">
+            <div className="row">
+                <div className="card col-md-4">
+                    <div className="card-header bg-warning text-center"><b>FrontEnd</b></div>
+                    <div className="card-body text-center">
+                        <div>HTML5</div>
+                        <div>CSS3</div>
+                        <div>JavaScript</div>
+                        <div>Bootstrap</div>
+                        <div>React</div>
+                        <div>Redux</div>
+                    </div>
+                </div>
+            
+                <div className="card col-md-4 ">
+                    <div className="card-header bg-warning text-center"><b>BackEnd</b></div>
+                    <div className="card-body text-center">
+                        <div>C#</div>
+                        <div>Entity Framework Core</div>
+                        <div>ASP.Net using WEB API</div>
+                        <div>DBMS</div>
+                        <div>Microsoft SQL Server</div>
+                    </div>
+                </div>
+        
+                <div className="card col-md-4">
+                    <div className="card-header bg-warning text-center"><b>Other</b></div>
+                    <div className="card-body text-center">
+                        <div>Postman</div>
+                        <div>Github</div>
+                        <div>Github Copilot</div>
+                        <div>Jira</div>
+                    </div>
+                </div>
+
+            </div>
+            </div>
+        </div>
         </>
     )
 }
--- src/component/Academics.js
+++ src/component/Academics.js
@@ -1,9 +1,38 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 export default function Academics(){
     return(
-        <>
-        <div><h1>You are in academics page</h1></div>
-        </>
+        <div className="container mt-5">
+            <div className="row">
+               <div className="col-md-6">
+                  <div className="card">
+                    <div className="card-header text-center bg-warning"><b>Post Graduation</b></div>
+                    <div className="card-body">
+                        <div>Degree:<b> Master In Technology (MTech)</b></div>
+                        <div>Stream:<b> Industrial Engineering and Managment</b></div>
+                        <div>College:<b> Visvesvaraya National Institute of Technology (VNIT, Nagpur)</b></div>
+                        <div>Project: <b>Process Capability Improvement in an Automobile Industry using DMAIC Methodology</b></div>
+                    </div>
+                    <div className="card-footer">2019-21</div>
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <div className="card">
+                      <div className="card-header text-center bg-warning"><b>Graduation</b></div>
+                      <div className="card-body">
+                        <div>Degree:<b> Bachelor In Engineering (B.E)</b></div>
+                        <div>Stream:<b> Mechanical Engineering</b></div>
+                        <div>College:<b> Kavikulguru Institute of Technology and Science (KITS, Nagpur)</b></div>
+                        <div>Project:<b> Design and Fabrication of Treadmill Bicycle</b></div>
+                        <div>&nbsp;</div>
+
+                      </div>
+                      <div className="card-footer">2014-18</div>
+                  </div>
+                </div>
+            </div>
+        </div>
     )
 }
